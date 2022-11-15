0d36694f6fc4efa5618f2ea464e226f33d945bc8
vvinerta <vinertacrespo@gmail.com>
2022-11-15 19:37:40 +0100

    Creacion del menu de opciones

--- app/src/main/java/com/example/tg1grupo3/ListaCitasActivity.java
+++ app/src/main/java/com/example/tg1grupo3/ListaCitasActivity.java
@@ -1,8 +1,16 @@
 package com.example.tg1grupo3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.Toast;
 
 public class ListaCitasActivity extends AppCompatActivity {
 
@@ -11,4 +19,24 @@ public class ListaCitasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_citas);
     }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.barraopciones,menu);
+        return  true;
+    }
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId()==R.id.nuevaCita){
+            abrirListaCitas();
+        }else if(item.getItemId()==R.id.calendario){
+            //aqui se debe de abrir el calendario
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void abrirListaCitas(){
+        Intent intento = new Intent(ListaCitasActivity.this,RegistrarCitaActivity.class);
+        startActivity(intento);
+    }
+
 }
