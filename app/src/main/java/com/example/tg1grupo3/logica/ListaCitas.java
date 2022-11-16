38708fe339c883690dded5cb3b406d86322f693b
Raúl Moya Gimeno <rmoya889@gmail.com>
2022-11-16 16:50:04 +0100

    Clase con la lista de las citas

--- app/src/main/java/com/example/tg1grupo3/logica/ListaCitas.java
+++ app/src/main/java/com/example/tg1grupo3/logica/ListaCitas.java
@@ -1,4 +1,24 @@
 package com.example.tg1grupo3.logica;
 
+import com.example.tg1grupo3.datos.Cita;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ListaCitas {
+
+    private static final List<Cita> citas = new ArrayList<>();
+
+    private ListaCitas(){    }
+
+    public static void addCita(Cita cita){
+        citas.add(cita);
+    }
+
+    public static List<Cita> filtrarPorFecha(String fecha){
+        return citas.stream()
+                .filter(cita -> cita.getFecha().equals(fecha))
+                .collect(Collectors.toList());
+    }
 }
