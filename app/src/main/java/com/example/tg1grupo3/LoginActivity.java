2d81c43de992791117353699af292582eb165fa7
Snowboys <ismaeljg2002@gmail.com>
2022-11-16 16:54:52 +0100

    login

--- app/src/main/java/com/example/tg1grupo3/LoginActivity.java
+++ app/src/main/java/com/example/tg1grupo3/LoginActivity.java
@@ -2,13 +2,41 @@ package com.example.tg1grupo3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText nombre;
+    private EditText contrasena;
+    private Button entrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        nombre = findViewById(R.id.nombre);
+        contrasena = findViewById(R.id.contrasena);
+        entrar = findViewById(R.id.btnentrar);
+
+        nombre.setHint("Usuario");
+        contrasena.setHint("Contraseña");
+
+
+
+        entrar.setOnClickListener(v ->{
+            if(ComprobarVacio(nombre)){nombre.setError("Debe introducir un usuario");}else if(ComprobarVacio(contrasena)){contrasena.setError("Debe introducir una contraseña");}else {
+                Intent intent = new Intent(getApplicationContext(), ListaCitasActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    static boolean ComprobarVacio(EditText campo){
+        return campo.getText().toString().trim().equals("");
     }
 }
