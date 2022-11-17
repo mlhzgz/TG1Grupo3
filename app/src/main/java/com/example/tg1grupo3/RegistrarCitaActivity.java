749d1c24a338c56b835e39879bacb4de5565ae9a
Alvaro <alvaropuyobraulio@gmail.com>
2022-11-17 21:06:16 +0100

    RegistrarCitas

--- app/src/main/java/com/example/tg1grupo3/RegistrarCitaActivity.java
+++ app/src/main/java/com/example/tg1grupo3/RegistrarCitaActivity.java
@@ -3,6 +3,9 @@ package com.example.tg1grupo3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.example.tg1grupo3.util.Utilidades;
 
 public class RegistrarCitaActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class RegistrarCitaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_cita);
+
+        EditText txtFechaCita = findViewById(R.id.txtFechaCita);
+
+
     }
 }
